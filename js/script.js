69f889ceca878df99fd527b02f3b21a45a474707
OlaideGrace <olaide.awonusi@yahoo.com>
2024-05-16 09:34:26 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const calculateOlaide = () => {};
+let displayValue = "";
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+  try {
+    let result = eval(displayValue);
+    document.getElementById("display").value = result;
+    displayValue = "";
+  } catch (error) {
+    document.getElementById("display").value = "error";
+  }
+}
